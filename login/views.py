22ff6f9229f96ff30372770061d29b976eb6f036
Matheus Alonso <matheus.siqueiraalonso@gmail.com>
2025-08-03 17:18:10 -0300

    chore: commit inicial

---
+++ login/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def login(request):
+    if request.method == "GET":        
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        dados = request.POST
+        print(dados)
+        return HttpResponse('Acessou!')
